66f67925836eca683d2905d816b93843453f6508
linzhanguca <lzhang12@uca.edu>
2022-03-04 15:35:32 -0600

    test p gain on left motor

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,14 @@ Speed measuring uses high frequency hardware timer 1Hz == 1ms) to measure the ti
 #define USE_TIMER_2 false
 #define USE_TIMER_3 false
 
-#define TIMER1_INTERVAL_MS 50 // 1s = 1000ms
+#define TIMER1_INTERVAL_MS 10 // 1s = 1000ms
 
 #include "TimerInterrupt_Generic.h"
 
+// Define PID
+const float K_p = 5;
+const float K_i = 0.01;
+const float K_d = 0.01;
 // Define left motor
 const byte leftPWM = 5; // make sure it is a PWM pin
 const byte leftIn1 = A0;
@@ -37,36 +41,60 @@ const byte leftEncA = 2;  // D2 reads left encoder's Ch.A
 const byte leftEncB = 3;  // D3 reads left encoder's Ch.B
 const byte rightEncA = 8; // D8 reads right encoder's Ch.A
 const byte rightEncB = 9; // D9 reads right encoder's Ch.B
-// const byte leftVcc = A5;  // A5 serves as Vcc for left encoder
-// const byte rightVcc = A6;  // A6 serves as Vcc for right encoder
+const byte leftVcc = A5;  // A5 serves as Vcc for left encoder
+const byte rightVcc = A6; // A6 serves as Vcc for right encoder
 
 // Define constans for robot
 const byte COUNTS_PER_REV = 12;
 const float GEAR_RATIO = 210.59;
-const float WHEEL_RADIUS = 0.021; // m
+const float WHEEL_RADIUS = 0.021;    // m
 const float WHEEL_SEPARATION = 0.09; // m
 
 // Define variables
-int8_t leftMotorDir = 0; // forward: 1; backward: -1
+int8_t leftMotorDir = 0;  // forward: 1; backward: -1
 int8_t rightMotorDir = 0; // forward: 1; backward: -1
 int32_t leftCounter = 0;
 int32_t rightCounter = 0;
 float leftCPS = 0.0;
 float rightCPS = 0.0;
-float linear = 0.0; 
-float angular = 0.0; 
+float leftRPS = 0.0;
+float rightRPS = 0.0;
+float linear = 0.0;
+float angular = 0.0;
+float targLinear = 0.2;
+float targAngular = 0.0;
+float targLeftRPS = 0.0;
+float targRightRPS = 0.0;
+float leftError = 0.0;
+byte leftDCycle = 0;
+int8_t leftDCycleInc = 0;
+byte rightDCyecle = 0;
 
 void TimerHandler1(void)
 {
-  leftCPS = leftMotorDir * leftCounter * 1000 / TIMER1_INTERVAL_MS; // counts per second
+  // Compute sensed velocity
+  leftCPS = leftMotorDir * leftCounter * 1000 / TIMER1_INTERVAL_MS; // WHEEL: counts per second
   rightCPS = rightMotorDir * rightCounter * 1000 / TIMER1_INTERVAL_MS;
-  linear = (leftCPS + rightCPS) / (COUNTS_PER_REV * GEAR_RATIO) * (2 * M_PI) * WHEEL_RADIUS / 2; // meters per second
-  angular = (rightCPS - leftCPS) / (COUNTS_PER_REV * GEAR_RATIO) * (2 * M_PI) * WHEEL_RADIUS / WHEEL_SEPARATION; // radians per second
+  leftRPS = leftCPS / (COUNTS_PER_REV * GEAR_RATIO) * (2 * M_PI); // WHEEL: radians per second
+  rightRPS = rightCPS / (COUNTS_PER_REV * GEAR_RATIO) * (2 * M_PI);
+  linear = (leftRPS + rightRPS) * WHEEL_RADIUS / 2; // ROBOT: meters per second
+  angular = (rightRPS - leftRPS) * WHEEL_RADIUS / WHEEL_SEPARATION;
+  // linear = (leftCPS + rightCPS) / (COUNTS_PER_REV * GEAR_RATIO) * (2 * M_PI) * WHEEL_RADIUS / 2; // meters per second
+  // angular = (rightCPS - leftCPS) / (COUNTS_PER_REV * GEAR_RATIO) * (2 * M_PI) * WHEEL_RADIUS / WHEEL_SEPARATION; // radians per second
+  // Debug
   if (TIMER_INTERRUPT_DEBUG > 1)
   {
     Serial.print(leftCPS);
     Serial.print(",");
-    Serial.println(rightCPS);
+    Serial.print(rightCPS);
+    Serial.print(",");
+    Serial.print(leftRPS);
+    Serial.print(",");
+    Serial.print(rightRPS);
+    Serial.print(",");
+    Serial.print(linear);
+    Serial.print(",");
+    Serial.println(angular);
   }
   leftCounter = 0;
   rightCounter = 0;
@@ -94,6 +122,11 @@ void ISR_countRightEncB(void)
 
 void setup()
 {
+  // Set pins to power up encoders
+  pinMode(leftVcc, OUTPUT);
+  pinMode(rightVcc, OUTPUT);
+  digitalWrite(leftVcc, HIGH);  // sets the analog pin A5 on
+  digitalWrite(rightVcc, HIGH); // sets the analog pin A6 on
   // Set all the motor control pins to outputs
   pinMode(leftPWM, OUTPUT);
   pinMode(rightPWM, OUTPUT);
@@ -141,6 +174,10 @@ void setup()
   digitalWrite(rightIn2, LOW);
   delay(1000);
 
+  // Init target vel
+  // float targLin = 0;
+  // float targAng = 0;
+
   // Initialize counter
   leftCounter = 0;
   rightCounter = 0;
@@ -148,25 +185,54 @@ void setup()
   // Set motor direction
   digitalWrite(leftIn1, HIGH);
   digitalWrite(leftIn2, LOW);
-  leftMotorDir = -1;
+  leftMotorDir = 1;
   digitalWrite(rightIn1, LOW);
   digitalWrite(rightIn2, HIGH);
-  rightMotorDir = 1;
+  rightMotorDir = -1;
 }
 
 void loop()
 {
+  // PID control
+  targLeftRPS = (targLinear - (targAngular * WHEEL_SEPARATION) / 2) / WHEEL_RADIUS; // radians per second
+  targRightRPS = (targLinear + (targAngular * WHEEL_SEPARATION) / 2) / WHEEL_RADIUS;
+  leftError = targLeftRPS - leftRPS;
+  leftDCycleInc = int(K_p * leftError);
+  leftDCycle += leftDCycleInc;
+  if (leftDCycle > 255)
+  {
+    leftDCycle = 255;
+  }
+  else if (leftDCycle < 0)
+  {
+    leftDCycle = 0;
+  }
+  analogWrite(leftPWM, leftDCycle);
+  Serial.print(targLeftRPS);
+  Serial.print(",");
+  Serial.println(leftRPS);
+  delay(20);
+
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   targLinear = float(i);
+  //   delay(1000);
+  // }
+  // for (int i = 10; i > 0; i--)
+  // {
+  //   targLinear = float(i);
+  //   delay(1000);
+  // }
   // Print speed
   // Serial.print("left cps: ");
   // Serial.print(leftCPS);
   // Serial.print(", right cps: ");
   // Serial.println(rightCPS);
-  Serial.print(linear);
-  Serial.print(",");
-  Serial.println(angular);
+  // Serial.print(linear);
+  // Serial.print(",");
+  // Serial.println(angular);
 
   // Drive motors
-  analogWrite(leftPWM, 100);
-  analogWrite(rightPWM, 100);
-  delay(10);
+  // analogWrite(leftPWM, 100);
+  // analogWrite(rightPWM, 100);
 }
